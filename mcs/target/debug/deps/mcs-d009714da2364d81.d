72700263901dd0fd7e45f5ed073e1b3e78deca62
Caden Siva <nedac27@gmail.com>
2024-03-06 14:18:51 -0500

    some initial commits after creation of repo

---
+++ mcs/target/debug/deps/mcs-d009714da2364d81.d
@@ -0,0 +1,5 @@
+/home/siva/code/oxidized-mcs/mcs/target/debug/deps/mcs-d009714da2364d81: src/main.rs
+
+/home/siva/code/oxidized-mcs/mcs/target/debug/deps/mcs-d009714da2364d81.d: src/main.rs
+
+src/main.rs:
---
+++ mcs/target/debug/mcs.d
@@ -0,0 +1,1 @@
+/home/siva/code/oxidized-mcs/mcs/target/debug/mcs: /home/siva/code/oxidized-mcs/mcs/src/main.rs
