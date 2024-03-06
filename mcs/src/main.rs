c1d798ac2d1fe3adcfb402998bbf5e287a0d02c8
Siva <nedac27@gmail.com>
2024-03-06 17:18:42 -0500

    first server test

--- mcs/src/main.rs
+++ mcs/src/main.rs
@@ -11,34 +11,37 @@ pub mod mc_datatypes;
 pub struct Server {
     host: String,
     port: u16,
-    server_socket: net::UdpSocket,
+    server_socket: net::TcpListener,
 }
 
 impl Server {
 
     pub fn new(host: String, port: u16) -> Server {
-                    
+        let falure = host.clone();
+        let tears = port.clone();
+        let sock_addr = host + ":" + &port.to_string();
+        
+        let socket = net::TcpListener::bind(sock_addr);
+
 
         Server {
-            host: host, 
-            port: port,
-            server_socket: net::UdpSocket::bind({
-                (host.to_string() + port.to_string())
-            }).expect("uh oh spaghettio")
+            host: falure, 
+            port: tears,
+            server_socket: socket.expect("uh oh spaghettio")
 
         }
     } 
 
 }
-
-
-
-
-
 fn main() ->  Result<()> {
 
     println!("Spooling Server...");
 
+    {
+        let host: String = "127.0.0.1".to_string();
+        let port: u16 = 5000;
+        let server = Server::new(host, port);
+    }
 
     
 
