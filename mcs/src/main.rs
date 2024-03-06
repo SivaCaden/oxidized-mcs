cfe39e971e0869de55c1ea054b95c9532c59e298
Siva <nedac27@gmail.com>
2024-03-06 17:28:21 -0500

    seperated server code from main.rs

--- mcs/src/main.rs
+++ mcs/src/main.rs
@@ -1,38 +1,13 @@
 
-use std::net;
 use std::io::{Result};
 
 pub mod mc_datatypes;
+pub mod server;
 
 
 
 
 
-pub struct Server {
-    host: String,
-    port: u16,
-    server_socket: net::TcpListener,
-}
-
-impl Server {
-
-    pub fn new(host: String, port: u16) -> Server {
-        let falure = host.clone();
-        let tears = port.clone();
-        let sock_addr = host + ":" + &port.to_string();
-        
-        let socket = net::TcpListener::bind(sock_addr);
-
-
-        Server {
-            host: falure, 
-            port: tears,
-            server_socket: socket.expect("uh oh spaghettio")
-
-        }
-    } 
-
-}
 fn main() ->  Result<()> {
 
     println!("Spooling Server...");
@@ -40,7 +15,7 @@ fn main() ->  Result<()> {
     {
         let host: String = "127.0.0.1".to_string();
         let port: u16 = 5000;
-        let server = Server::new(host, port);
+        let server = server::Server::new(host, port);
     }
 
     
---
+++ mcs/src/server.rs
@@ -0,0 +1,27 @@
+use std::net;
+
+pub struct Server {
+    host: String,
+    port: u16,
+    server_socket: net::TcpListener,
+}
+
+impl Server {
+
+    pub fn new(host: String, port: u16) -> Server {
+        let falure = host.clone();
+        let tears = port.clone();
+        let sock_addr = host + ":" + &port.to_string();
+        
+        let socket = net::TcpListener::bind(sock_addr);
+
+
+        Server {
+            host: falure, 
+            port: tears,
+            server_socket: socket.expect("uh oh spaghettio")
+
+        }
+    } 
+
+}
