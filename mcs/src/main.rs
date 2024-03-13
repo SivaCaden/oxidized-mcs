1456167b2fb589ba68e1f36aad2b9355cf7ec20e
Siva <nedac27@gmail.com>
2024-03-12 23:10:22 -0400

    drunk commit

--- mcs/src/main.rs
+++ mcs/src/main.rs
@@ -1,14 +1,23 @@
 use std::{
     io::{ prelude::*, BufReader, Result, Error},
     net::{TcpListener, TcpStream},
+    collections::HashMap,
 };
 use std::fs;
-use etherparse::*;
 
 pub mod mc_datatypes;
 
 
 
+pub struct PubKeyInfo {
+    sequence: String,
+    algorithm: String,
+    parameters: String,
+}
+pub struct PubKey {
+    modulus: usize,
+    public_exponent: usize,
+}
 
 
 
@@ -34,6 +43,26 @@ fn main() ->  Result<()> {
     Ok(())
 
 }
+pub struct PackType {
+    pub header: String,
+    pub data_vec: Vec<String>,
+}
+
+
+pub enum PacketType {
+    JoinGame (String, Vec<String>),
+    PluginMessage (String, Vec<String>),
+    ServerDifficutly (String, Vec<String>),
+    PlayerAbilitys (String, Vec<String>),
+    PlayerPositionAndLook (String, Vec<String>),
+    KeepAlive (String, Vec<String>),
+    TimeAlive (String, Vec<String>),
+    ChatMessage (String, Vec<String>), 
+}
+const JOINGAME = ( "join_game".to_string(), vec!["i32", "u8", "u8", "u8", "u8", "u8", "bool;
+
+
+
 
 
 pub fn handle_connection( mut stream: TcpStream ) {
@@ -42,16 +71,8 @@ pub fn handle_connection( mut stream: TcpStream ) {
     let mut buf: Vec<u8> = Vec::new();
 
     let data = buf_reader.read(&mut buf);
+
     
-    match data {
-        Ok(fuck) => {
-            println!("Data received");
-            
-        },
-        Err(e) => {
-            println!("Error: {}", e);
-        }
-    }
 
     let size = buf.len();
 
@@ -65,4 +86,11 @@ pub fn handle_connection( mut stream: TcpStream ) {
     
 
     
+}
+
+
+fn start_encryption() -> T {
+    let mut data = String::new();
+
+
 }
